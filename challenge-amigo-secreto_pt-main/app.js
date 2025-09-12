e2478ee20926f7d6ae4371bbdc277f7364feef6d
Maria Eduarda Santana <mariaedufsantana@gmail.com>
2025-09-12 17:46:25 -0300

    Adicionando funcionalidades para adicionar os amigos e mostrar a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.querySelector("#nomeAmigo");
+    let nome = input.value; 
+
+
+    if (nome == "") {
+        alert("Digite um nome.");
+        return;
+    } 
+
+    amigos.push(nome); //adicionar o nome na lista
+    input.value = ""; //limpar o campo pra digitar novamente
+
+    mostrarLista(); //função para saber quem á está na lista. 
+}
+
+//atualizando os amigos na tela
+
+function mostrarLista (){
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML = ""; //serve para limpar a lita antes de atualizar
+    lista.innerHTML = "<li>" + amigos.join("</li><li>") + "</li>"; // exibir os amigos
+}
